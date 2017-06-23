fc47e1ba3a8c9477ddd60f459dd150576cefa493
Thiago <thiagoqdrb@gmail.com>
2017-06-23 15:23:28 -0400

    edits to user model

--- amazia_app_node/models/restaurant.js
+++ amazia_app_node/models/restaurant.js
@@ -46,14 +46,16 @@ Restaurant.update = (restaurant, id) => {
          type = $4,
          address = $5,
          rating = $6,
-         area = $7
-        WHERE id = $8
+         user_id = $7,
+         area = $8
+        WHERE id = $9
     `, [restaurant.restaurant_name,
         restaurant.img_url,
         restaurant.description,
         restaurant.type,
         restaurant.address,
         restaurant.rating,
+        restaurant.user_id,
         restaurant.area,
         id]);
 }
